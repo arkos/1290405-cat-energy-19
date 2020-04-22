b557fc6c8d3b7f8a82d89fe97e6c27233df4cd74
Kostya Kaganov <kkaganov@gmail.com>
2020-04-22 21:23:01 +0300

    Add build gulp job and update the build task in package.json

--- gulpfile.js
+++ gulpfile.js
@@ -80,4 +80,5 @@ gulp.task("server", function () {
   gulp.watch("source/*.html").on("change", server.reload);
 });
 
-gulp.task("start", gulp.series("css", "server"));
+gulp.task("build", gulp.series("css", "sprite", "html"));
+gulp.task("start", gulp.series("build", "server"));
